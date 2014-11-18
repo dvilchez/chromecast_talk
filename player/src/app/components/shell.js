2c2386a8b7d5cfc1791bc7e2c1e98b3e7417ba80
David Vilchez <dvilchez@xuaps.com>
2014-11-18 13:41:57 +0100

    hello world

---
+++ player/src/app/components/shell.js
@@ -0,0 +1,19 @@
+/** @jsx React.DOM */
+
+'use strict';
+var React = require('react');
+
+var Shell = React.createClass({
+    render: function() {
+        return (
+        	/*jshint ignore:start */
+
+            <div>
+            	<h2>Hello, World</h2>
+            </div>
+            /*jshint ignore:end */
+        );
+    }
+});
+
+module.exports = Shell;
