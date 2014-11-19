0c1bf0b399174c8249a1ed8acd679bb831930aaf
David Vilchez <dvilchez@xuaps.com>
2014-11-19 12:56:48 +0100

    iitialize api

--- player/src/app/app.js
+++ player/src/app/app.js
@@ -19,3 +19,24 @@ React.render(
 );
 
 window.React = React;
+window.__onGCastApiAvailable = function(loaded, errorInfo){
+    if(loaded){
+        var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
+        var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+            sessionListener,
+            receiverListener);
+        chrome.cast.initialize(apiConfig, function(){ console.log('init success'); }, function(){ console.log('init error'); }) ;
+    }else{
+        console.log(errorInfo);
+    }
+};
+
+function sessionListener(e){
+
+};
+
+function receiverListener(e){
+    if( e === chrome.cast.ReceiverAvailability.AVAILABLE) {
+    
+    }
+};
